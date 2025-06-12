fff135f9d9cd143c0d689a2126b88bd0d54651cb
Boluwatife <bolu.onukwu@gmail.com>
2025-06-12 01:47:40 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,8 @@ function App() {
 export default App
  */
 "use client";
-import React from "react";
+import React, { memo } from "react";
+import me from "./assets/me.jpg";
 
 import ProfileCard from "./ProfileCard";
 import "./App.css";
@@ -25,7 +26,7 @@ function App() {
     <main className="container">
       <ProfileCard
         name="Onukwu Boluwatife Ifeanyichukwu"
-        image="/src/assets/me.jpg"
+        image={me}
         bio="Frontend developer passionate about creating beautiful user interfaces. I love working with React and exploring new technologies. In my free time, I enjoy reading and listening to music."
       />
     </main>
